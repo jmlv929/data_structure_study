c4bfe431eff9a6baf21cccfe4322438b580e6625
DESKTOP-KG2UIRC\72721 <727216411@qq.com>
2022-04-10 17:14:27 +0800

    Visual Studio环境配置

---
+++ My_Code/Ch2/sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	int i, sum = 0;
+	for (i = 1; i < 101; i++) {
+		sum = sum + i;
+	}
+	printf("%d\n", sum);
+
+	int n = 100;
+	printf("%d", sum_new(n));
+}
+
+int sum_new(int n) {
+	int sum;
+	sum = (1 + n) * n / 2;
+	return sum;
+}
